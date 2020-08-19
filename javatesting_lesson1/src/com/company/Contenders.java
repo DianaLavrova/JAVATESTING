68f2c05effb9e5eff32dbc5ed8455f2ead4cea76
diana <dianalavrova92@gmail.ru>
2020-08-19 20:17:10 +0300

    deleted lesson1 master clear

--- javatesting_lesson1/src/com/company/Contenders.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public interface Contenders {
-    public void jump();
-    public void jump(Wall wall);
-    public void run();
-    public void run (RunWay runWay);
-}
--- javatesting_lesson1/src/com/company/RunWay.java
+++
@@ -1,5 +0,0 @@
-package com.company;
-
-public class RunWay {
-    public int lenght = 15;
-}
